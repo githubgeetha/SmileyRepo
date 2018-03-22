9458fdd00f2d96eac68f7d19b43517349a1302fa
GEETA <GEETA@LAPTOP-8O5TVBBK>
2018-03-22 12:52:38 +0530

    New Pojo class added

---
+++ src/com/pack1/Person.java
@@ -0,0 +1,21 @@
+package com.pack1;
+
+public class Person
+{
+	int phno;
+	String name;
+	public int getPhno() {
+		return phno;
+	}
+	public void setPhno(int phno) {
+		this.phno = phno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
